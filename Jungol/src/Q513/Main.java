e50f1438cfbe2ebe026b83646ab0f4c5c1d20749
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:31:49 +0900

    Q513

---
+++ Jungol/src/Q513/Main.java
@@ -0,0 +1,14 @@
+package Q513;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double yd = 91.44;
+		double in = 2.54;
+		
+		System.out.printf(" 2.1yd = %.1fcm \r\n", 2.1 * yd);
+		System.out.printf("10.5in =  %.1fcm", 10.5 * in);
+	}
+
+}
