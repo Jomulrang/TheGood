3959e3db3daaa644e240fdd2eb8187dc75915bb3
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 16:42:02 +0900

    first git add

---
+++ Jungol/src/q510/Main.java
@@ -0,0 +1,13 @@
+package q510;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("전체 7자리로 맞추고 소수 4자리까지 출력\r\n"
+				+ "");
+		double x = 1.2340;
+		double y = 10.3459;
+		
+	}
+}
---
+++ Jungol/src/q518/Main.java
@@ -0,0 +1,26 @@
+package q518;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+//		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		sc.close();
+		
+//		System.out.printf("%d", a+b);
+		
+		int a = 10;
+		int b = 10;
+		System.out.printf("최솟값 a = %d, b = %d\r\n", a, b);
+		
+		
+		
+		
+		
+		
+		
+	}
+}
