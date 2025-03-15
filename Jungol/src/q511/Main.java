0f8c5aa834436534f1f78ec4179ba2d5a6ad997c
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:18:43 +0900

    q511

---
+++ Jungol/src/q511/Main.java
@@ -0,0 +1,13 @@
+package q511;
+
+	public class Main {
+		public static void main(String[] args) {
+			int a = 55;
+			int b = 10;
+			System.out.printf("%d - %d = %d", a, b, a-b);
+			System.out.println();
+			int c = 2008;
+			int d = 1999;
+			System.out.printf("%d - %d = %d", c, d, c-d);
+	}
+}
